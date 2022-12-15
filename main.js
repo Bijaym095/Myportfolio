c268eee126bcd08730daf1ecb04c7b3097991bcd
Bijay <Bijaym095@gmail.com>
2022-12-15 20:11:45 +0545

    new sections added

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const navigation = document.querySelector("#navcontainer");
 const navLinks = document.querySelectorAll(".nav-link");
 const linksContainer = document.querySelector(".navbar-collapse");
+const navHeight = navigation.getBoundingClientRect().height;
 
+//sticky navbar
 window.addEventListener("scroll", () => {
   const position = window.scrollY;
-  const navHeight = navigation.getBoundingClientRect().height;
   if (position > navHeight) {
     navigation.classList.add("sticky");
   } else {
@@ -12,8 +13,29 @@ window.addEventListener("scroll", () => {
   }
 });
 
-navLinks.forEach((link) =>
+//active menu class while scrolling the window
+window.addEventListener("scroll", () => {
+  const scrollPosition = scrollY;
+  const sections = document.querySelectorAll("section");
+  let current = "";
+  sections.forEach((section) => {
+    if (scrollPosition >= section.offsetTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  //removing active link from all nav-links as default
+  if (current) {
+    navLinks.forEach((li) => {
+      li.classList.remove("active-link");
+      if (li.classList.contains(current)) {
+        li.classList.add("active-link");
+      }
+    });
+  }
+});
+
+navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     linksContainer.classList.remove("show");
-  })
-);
+  });
+});
